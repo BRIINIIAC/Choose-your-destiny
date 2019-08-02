31fd973699b7de887741b4e981247f9b6364e70e
Boss <lenovolenovo_1310@bk.ru>
2019-08-03 00:17:18 +0600

    Это наш первый комит

---
+++ skr.js
@@ -0,0 +1,28 @@
+
+'use strict';                                  
+                                    /*Js с пользователем */
+                                
+
+/*let ans = prompt("have you 18", "Yes");
+                                     
+                                     /* typeof преоброзует числа в строку */
+
+/*console.log(typeof(ans)); */
+
+
+let arr = 10,
+    dec = 15;
+
+arr++;
+dec--;
+console.log(arr);
+console.log(dec);
+
+
+
+
+
+
+
+
+
